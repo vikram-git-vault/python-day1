9071aadc471819945ee1a5f5f05236006448005f
Vikram <vikramhere@gmail.com>
2026-06-22 16:44:35 +0530

    Day 1 Excercises completed

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+print ("Hello, World!")
+print ("Welcome to Python programming.")
+print ("This is a simple Python script.")
+name = "Vikram"
+print ("My name is " + name + ".")
+age = 45
+print ("I am " + str(age) + " years old.")
+for i in range(1, 11):
+    print (i)
+movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight"]
+for movie in movies:
+    print ("I like the movie: " + movie)
+person = {
+    "name": "Vikram",
+    "city": "Chennai",
+    "age": 45
+}
+print ("My name is " + person["name"] + ", I live in " + person["city"] + ", and I am " + str(person["age"]) + " years old.")
